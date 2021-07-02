6332efff35fa148aa63f6c125ad1ebe04c6325c9
Michael LeCompte <michael.lecompte@rover.com>
2021-07-02 02:51:24 -0700

    feat: add vimscript and lua lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,19 @@
 local lspconf = require('lspconfig')
 
 lspconf.pyls.setup {}
+lspconf.vimls.setup {}
+
+
+local lls_root = '/Users/michael/projects/lls/'
+local lls_bin = lls_root..'bin/macOS/lua-language-server'
+
+lspconf.sumneko_lua.setup {
+	cmd = {lls_bin, '-E', lls_root..'/main.lua'};
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {'vim'},
+			},
+		},
+	},
+}
